37b32140402d13f9cde902b2c02a0c566f909181
Iosim Liviu <liviuiosim@gmail.com>
2020-12-23 20:12:12 +0200

    made create post endpoint and added text editor for recipe

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const postsController = require("../controllers/posts.js");
 
+router.post('/', postsController.createPost);
 router.get('/', postsController.getAllPosts);
 router.get('/users/:userId', postsController.getPostsForUser);
 
