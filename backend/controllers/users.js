f78eb4f6d0f9721044d14c417362d35cbe51c107
Iosim Liviu <liviuiosim@gmail.com>
2020-12-25 16:23:16 +0200

    get and delete user by id

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,16 +1,58 @@
-const firebase = require('../services/firebase.js')
+const firebase = require("../services/firebase.js");
 
 const getAllUsers = (req, res) => {
-    let users = []
-    firebase.admin.firestore().collection('users').get().then(snapshot => {
-        snapshot.forEach((doc) => {
-            users.push(doc.data())
-        });
-        res.status(200).send(users)
+  let users = [];
+  firebase.admin
+    .firestore()
+    .collection("users")
+    .get()
+    .then(snapshot => {
+      snapshot.forEach(doc => {
+        users.push(doc.data());
+      });
+      res.status(200).send(users);
     });
-}
-
-module.exports = {
-    getAllUsers
 };
 
+const getUser = async (req, res) => {
+  try {
+    let userId = req.params.userId;
+    const userRef = firebase.admin
+      .firestore()
+      .collection("users")
+      .doc(userId);
+    const doc = await userRef.get();
+
+    if (!doc.exists) {
+      res.status(500).send({ message: "User not found" });
+    } else {
+      let user = doc.data();
+      res.status(200).send(user);
+    }
+  } catch (e) {
+    console.error(e);
+    res.status(500).send({ message: "Get user failed" });
+  }
+};
+
+const deleteUser = async (req, res) => {
+  try {
+    let userId = req.params.userId;
+    await firebase.admin
+      .firestore()
+      .collection("users")
+      .doc(userId)
+      .delete();
+
+    res.status(200).send(`User was deleted`);
+  } catch (e) {
+    console.error(e);
+    res.status(500).send({ message: "Delete user failed" });
+  }
+};
+
+module.exports = {
+  getAllUsers,
+  getUser,
+  deleteUser
+};
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const usersController = require("../controllers/users.js");
 
-router.get('/', usersController.getAllUsers);
+router.get("/", usersController.getAllUsers);
+router.get("/:userId", usersController.getUser);
+router.delete("/:userId", usersController.deleteUser);
 
 module.exports = router;
